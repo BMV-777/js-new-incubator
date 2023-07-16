6fd487eae3873a64ed0a36aca1a262964669f5c9
Maks Borenkov <borenkovmv@gmail.com>
2023-07-16 19:05:07 +0300

    if

--- main.js
+++ main.js
@@ -1,15 +1,59 @@
-let cards = [
-  { id: 1, balance: 5000, currency: "USD" },
-  { id: 2, balance: 4000, currency: "EVR" },
-  { id: 3, balance: 2000, currency: "UA" },
-];
+// let cards = [
+//   { id: 1, balance: 5000, currency: "USD" },
+//   { id: 2, balance: 4000, currency: "EVR" },
+//   { id: 3, balance: 2000, currency: "UA" },
+// ];
 
-function handelClick() {
-  cards.forEach((card) => {
-    console.log(
-      `Card Id: ${card.id}, Balance ${card.balance}, Currency: ${card.currency}`
-    );
-  });
-}
+// function handelClick() {
+//   cards.forEach((card) => {
+//     console.log(
+//       `Card Id: ${card.id}, Balance ${card.balance}, Currency: ${card.currency}`
+//     );
+//   });
+// }
 
-document.querySelector("button").addEventListener("click", handelClick);
+// document.querySelector("button").addEventListener("click", handelClick);
+
+// const word0 = "Good";
+// const word0translate = "Отлично";
+
+// const word1 = "Boy";
+// const word1translate = "Парень";
+
+// const word2 = "Pragrammist";
+// const word2translate = "Праграммист";
+
+// const wordLeng = 3;
+
+// const finishGoodMessage = "Молодец. Good test";
+// const finishFalseMessage = "Молодец. папробуй еще все получится!";
+
+// let counter = 0;
+
+// const procentTest = 50;
+
+// const userAnswer0 = prompt(word0);
+// alert(userAnswer0 === word0translate);
+// if (userAnswer0 === word0translate) {
+//   counter = counter += 1;
+// }
+
+// const userAnswer1 = prompt(word1);
+// alert(userAnswer1 === word1translate);
+// if (userAnswer1 === word1translate) {
+//   counter = counter += 1;
+// }
+
+// const userAnswer2 = prompt(word2);
+// alert(userAnswer2 === word2translate);
+// if (userAnswer2 === word2translate) {
+//   counter = counter += 1;
+// }
+
+// const countWordleng = (counter / wordLeng) * 100;
+
+// if (countWordleng > procentTest) {
+//   alert(finishGoodMessage);
+// } else {
+//   alert(finishFalseMessage);
+// }
