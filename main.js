fa1dc4f07b777d2e38574ad737df072c8b7f6297
Maks Borenkov <borenkovmv@gmail.com>
2023-07-28 19:04:11 +0300

    urok10

--- main.js
+++ main.js
@@ -32,20 +32,20 @@
 
 // const procentTest = 50;
 
-// const userAnswer0 = prompt(word0);
-// alert(userAnswer0 === word0translate);
+// const userAnswer0 = window.prompt(word0);
+// document.write(userAnswer0);
 // if (userAnswer0 === word0translate) {
 //   counter = counter += 1;
 // }
 
-// const userAnswer1 = prompt(word1);
-// alert(userAnswer1 === word1translate);
+// const userAnswer1 = window.prompt(word1);
+// document.write(userAnswer1 === word1translate);
 // if (userAnswer1 === word1translate) {
 //   counter = counter += 1;
 // }
 
-// const userAnswer2 = prompt(word2);
-// alert(userAnswer2 === word2translate);
+// const userAnswer2 = window.prompt(word2);
+// document.write(userAnswer2 === word2translate);
 // if (userAnswer2 === word2translate) {
 //   counter = counter += 1;
 // }
@@ -57,3 +57,76 @@
 // } else {
 //   alert(finishFalseMessage);
 // }
+
+// document.write("Start", "<br>");
+
+// if (true) {
+//   document.write("Product push");
+// } else {
+//   document.write("Not product");
+// }
+
+// document.write("<br>", "END");
+
+// let balance = 110;
+
+// // if (balance >= productPrice) {
+// //   console.log("Вы приобрели этот товар");
+// // } else {
+// //   console.log("Не достаточно средств на счету!!!");
+// // }
+// const productPriceString1 = prompt("How are you product");
+
+// const productPrice1 = Number(productPriceString1);
+
+// // if (productPrice1 < balance) {
+// //   console.log("Не достаточно средств на счету!!!");
+// // }
+
+// if (productPrice1 === productPrice1) {
+//   if (balance < productPriceString1) {
+//     console.log("Не достаточно средств на счету!!!");
+//   } else {
+//     console.log("Вы приобрели этот товар");
+//     balance -= productPrice1;
+//     console.log("Осталося на счету: ", balance);
+//   }
+// } else {
+//   console.log("Is not coercion input");
+// }
+// // console.log(balance);
+
+// const productPriceString2 = prompt("How are you product");
+// const productPrice2 = Number(productPriceString2);
+
+// if (productPrice2 === productPrice2) {
+//   if (balance < productPriceString2) {
+//     console.log("Не достаточно средств на счету!!!");
+//   } else {
+//     console.log("Вы приобрели этот товар");
+//     balance -= productPrice2;
+//     console.log("Осталося на счету: ", balance);
+//   }
+// } else {
+//   console.log("Is not coercion input");
+// }
+
+// const productPriceString3 = prompt("How are you product");
+// const productPrice3 = Number(productPriceString2);
+
+// if (productPrice3 === productPrice3) {
+//   if (balance < productPriceString3) {
+//     console.log("Не достаточно средств на счету!!!");
+//   } else {
+//     console.log("Вы приобрели этот товар");
+//     balance -= productPrice3;
+//     console.log("Осталося на счету: ", balance);
+//   }
+// } else {
+//   console.log("Is not coercion input");
+// }
+
+const product1 = 100;
+const product2 = 200;
+
+console.log(product1 < product2);
